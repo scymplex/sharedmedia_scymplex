2223b1ef8be3070ff131bf576d6062b106165515
scymplex <58167539+scymplex@users.noreply.github.com>
2023-09-07 12:04:12 -0700

    Add files via upload

---
+++ SharedMedia_Scymplex/SharedMedia_Scymplex.lua
@@ -0,0 +1,54 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "AnonymousPro-Bold", [[Interface\Addons\SharedMedia_Scymplex\font\AnonymousPro-Bold.ttf]]) 
+LSM:Register("font", "AnonymousPro-Regular", [[Interface\Addons\SharedMedia_Scymplex\font\AnonymousPro-Regular.ttf]]) 
+LSM:Register("font", "DejaVuSansMono-Bold", [[Interface\Addons\SharedMedia_Scymplex\font\DejaVuSansMono-Bold.ttf]]) 
+LSM:Register("font", "DejaVuSansMono-BoldOblique", [[Interface\Addons\SharedMedia_Scymplex\font\DejaVuSansMono-BoldOblique.ttf]]) 
+LSM:Register("font", "DejaVuSansMono-Oblique", [[Interface\Addons\SharedMedia_Scymplex\font\DejaVuSansMono-Oblique.ttf]]) 
+LSM:Register("font", "DejaVuSansMono", [[Interface\Addons\SharedMedia_Scymplex\font\DejaVuSansMono.ttf]]) 
+LSM:Register("font", "exprswy_free", [[Interface\Addons\SharedMedia_Scymplex\font\exprswy_free.ttf]]) 
+LSM:Register("font", "IBMPlexMono-Bold", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-Bold.ttf]]) 
+LSM:Register("font", "IBMPlexMono-BoldItalic", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-BoldItalic.ttf]]) 
+LSM:Register("font", "IBMPlexMono-ExtraLight", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-ExtraLight.ttf]]) 
+LSM:Register("font", "IBMPlexMono-ExtraLightItalic", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-ExtraLightItalic.ttf]]) 
+LSM:Register("font", "IBMPlexMono-Italic", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-Italic.ttf]]) 
+LSM:Register("font", "IBMPlexMono-Light", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-Light.ttf]]) 
+LSM:Register("font", "IBMPlexMono-LightItalic", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-LightItalic.ttf]]) 
+LSM:Register("font", "IBMPlexMono-Medium", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-Medium.ttf]]) 
+LSM:Register("font", "IBMPlexMono-MediumItalic", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-MediumItalic.ttf]]) 
+LSM:Register("font", "IBMPlexMono-Regular", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-Regular.ttf]]) 
+LSM:Register("font", "IBMPlexMono-SemiBold", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-SemiBold.ttf]]) 
+LSM:Register("font", "IBMPlexMono-SemiBoldItalic", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-SemiBoldItalic.ttf]]) 
+LSM:Register("font", "IBMPlexMono-Thin", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-Thin.ttf]]) 
+LSM:Register("font", "IBMPlexMono-ThinItalic", [[Interface\Addons\SharedMedia_Scymplex\font\IBMPlexMono-ThinItalic.ttf]]) 
+LSM:Register("font", "X", [[Interface\Addons\SharedMedia_Scymplex\font\X.ttf]]) 
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "bloodlust_ready", [[Interface\Addons\SharedMedia_Scymplex\sound\bloodlust_ready.ogg]]) 
+LSM:Register("sound", "bop", [[Interface\Addons\SharedMedia_Scymplex\sound\bop.ogg]]) 
+LSM:Register("sound", "clear", [[Interface\Addons\SharedMedia_Scymplex\sound\clear.ogg]]) 
+LSM:Register("sound", "done_eatting", [[Interface\Addons\SharedMedia_Scymplex\sound\done_eatting.ogg]]) 
+LSM:Register("sound", "eatting", [[Interface\Addons\SharedMedia_Scymplex\sound\eatting.ogg]]) 
+LSM:Register("sound", "healer_drinking", [[Interface\Addons\SharedMedia_Scymplex\sound\healer_drinking.ogg]]) 
+LSM:Register("sound", "incarn_ready", [[Interface\Addons\SharedMedia_Scymplex\sound\incarn_ready.ogg]]) 
+LSM:Register("sound", "one_spawned", [[Interface\Addons\SharedMedia_Scymplex\sound\one_spawned.ogg]]) 
+LSM:Register("sound", "potion_ready", [[Interface\Addons\SharedMedia_Scymplex\sound\potion_ready.ogg]]) 
+LSM:Register("sound", "power_infusion", [[Interface\Addons\SharedMedia_Scymplex\sound\power_infusion.ogg]]) 
+LSM:Register("sound", "two_spawned", [[Interface\Addons\SharedMedia_Scymplex\sound\two_spawned.ogg]]) 
+
+-- -----
+--   STATUSBAR
+-- -----
